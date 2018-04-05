9f594a67fd44263525c71c67e87ea947db4e19bc
Shekhar Patnaik <patnaikshekhar@gmail.com>
2018-04-05 16:28:05 +0100

    Added plugins to init script

--- init.groovy
+++ init.groovy
@@ -2,6 +2,7 @@
 
 import jenkins.model.*
 import hudson.security.*
+import java.util.logging.Logger
 
 def instance = Jenkins.getInstance()
 
@@ -14,6 +15,41 @@ instance.setSecurityRealm(hudsonRealm)
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
 instance.save()
+
+def logger = Logger.getLogger("")
+def installed = false
+def initialized = false
+def pluginParameter="blueocean"
+def plugins = pluginParameter.split()
+logger.info("" + plugins)
+
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
+plugins.each {
+  logger.info("Checking " + it)
+  if (!pm.getPlugin(it)) {
+    logger.info("Looking UpdateCenter for " + it)
+    if (!initialized) {
+      uc.updateAllSites()
+      initialized = true
+    }
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+      logger.info("Installing " + it)
+    	def installFuture = plugin.deploy()
+      while(!installFuture.isDone()) {
+        logger.info("Waiting for plugin install: " + it)
+        sleep(3000)
+      }
+      installed = true
+    }
+  }
+}
+if (installed) {
+  logger.info("Plugins installed, initializing a restart!")
+  instance.save()
+  instance.restart()
+}
 
 // import jenkins.model.*
 // import hudson.security.*
